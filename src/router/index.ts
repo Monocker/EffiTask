e843dd77c5f8ac5eb89e82c418b5b28ea1e129d3
Monocker <36136678+Monocker@users.noreply.github.com>
2024-03-16 22:58:43 -0500

    Feat, Login Maquetación. (#1)

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,6 @@ import { createRouter, createWebHistory } from 'vue-router'
 /* import HomeView from '../views/HomeView.vue' */
 /* import {  } from "../views/home/HomeView"; */
 import HomeView from '../views/home/HomeView.vue'
-
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -18,6 +17,12 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/login',
+      name: 'login',
+      // Importación dinámica para carga diferida
+      component: () => import('../views/authentication/LoginView.vue')
     }
   ]
 })
