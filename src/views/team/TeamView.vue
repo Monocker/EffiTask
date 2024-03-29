7e7706c78fb2aaffe89d35724d3370a51ff46844
Monocker <monocker@icloud.com>
2024-03-29 15:41:50 -0500

    Fix, Gerarquización de archivos y modificacion de nombre de componentes de Dashboard

--- src/views/team/TeamView.vue
+++ src/views/team/TeamView.vue
@@ -1,8 +1,7 @@
 <template>
     <div class="min-h-full">
-        <!-- Hola desde el Team View -->
-        <NavBarComponent/>
-        
+        <NavBarComponent />
+
         <div class="lg:col-span-5 xl:col-span-6 flex flex-col">
             <div class="relative z-10 rounded-xl bg-white shadow-xl overflow-hidden my-auto xl:mt-18">
                 <section>
@@ -71,16 +70,15 @@
 }
 </style>
 
-<script >
-//import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue'
-//import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/vue/24/outline'
-import NavBarComponent from '../../components/home/dashboard/NavBarComponent.vue'
+<script>
+
+import NavBarComponent from '../../components/shared/NavBarComponent.vue'
 import { ref, onMounted } from 'vue';
 
-// Define la variable reactiva dentro de setup
+
 let isMenuOpen = ref(false);
 
-// Cerrar el menú cuando se hace clic fuera de él
+
 onMounted(() => {
     document.addEventListener('click', closeMenuOnClickOutside);
 });
@@ -94,83 +92,82 @@ const closeMenuOnClickOutside = (event) => {
 };
 
 export default {
-  components: {
-    NavBarComponent
-  },
-  data() {
-    return {
-      user: {
-        name: 'Tom Cook',
-        email: 'tom@example.com',
-        imageUrl: 'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-      },
-      navigation: [
-        { name: 'Dashboard', href: '/dashboard', current: false },
-        { name: 'Team', href: '/team', current: true },
-        // más elementos de navegación...
-      ],
-      userNavigation: [
-        { name: 'Your Profile', href: '#' },
-        { name: 'Settings', href: '#' },
-        { name: 'Sign out', href: '#' },
-      ],
-      people: [
-    {
-        name: 'Leslie Alexander',
-        email: 'leslie.alexander@example.com',
-        role: 'Co-Founder / CEO',
-        imageUrl:
-            'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-        lastSeen: '3h ago',
-        lastSeenDateTime: '2023-01-23T13:23Z',
+    components: {
+        NavBarComponent
     },
-    {
-        name: 'Michael Foster',
-        email: 'michael.foster@example.com',
-        role: 'Co-Founder / CTO',
-        imageUrl:
-            'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-        lastSeen: '3h ago',
-        lastSeenDateTime: '2023-01-23T13:23Z',
+    data() {
+        return {
+            user: {
+                name: 'Tom Cook',
+                email: 'tom@example.com',
+                imageUrl: 'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+            },
+            navigation: [
+                { name: 'Dashboard', href: '/dashboard', current: false },
+                { name: 'Team', href: '/team', current: true },
+                // más elementos de navegación...
+            ],
+            userNavigation: [
+                { name: 'Your Profile', href: '#' },
+                { name: 'Settings', href: '#' },
+                { name: 'Sign out', href: '#' },
+            ],
+            people: [
+                {
+                    name: 'Leslie Alexander',
+                    email: 'leslie.alexander@example.com',
+                    role: 'Co-Founder / CEO',
+                    imageUrl:
+                        'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+                    lastSeen: '3h ago',
+                    lastSeenDateTime: '2023-01-23T13:23Z',
+                },
+                {
+                    name: 'Michael Foster',
+                    email: 'michael.foster@example.com',
+                    role: 'Co-Founder / CTO',
+                    imageUrl:
+                        'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+                    lastSeen: '3h ago',
+                    lastSeenDateTime: '2023-01-23T13:23Z',
+                },
+                {
+                    name: 'Dries Vincent',
+                    email: 'dries.vincent@example.com',
+                    role: 'Business Relations',
+                    imageUrl:
+                        'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+                    lastSeen: null,
+                },
+                {
+                    name: 'Lindsay Walton',
+                    email: 'lindsay.walton@example.com',
+                    role: 'Front-end Developer',
+                    imageUrl:
+                        'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+                    lastSeen: '3h ago',
+                    lastSeenDateTime: '2023-01-23T13:23Z',
+                },
+                {
+                    name: 'Courtney Henry',
+                    email: 'courtney.henry@example.com',
+                    role: 'Designer',
+                    imageUrl:
+                        'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+                    lastSeen: '3h ago',
+                    lastSeenDateTime: '2023-01-23T13:23Z',
+                },
+                {
+                    name: 'Tom Cook',
+                    email: 'tom.cook@example.com',
+                    role: 'Director of Product',
+                    imageUrl:
+                        'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+                    lastSeen: null,
+                },
+            ]
+        }
     },
-    {
-        name: 'Dries Vincent',
-        email: 'dries.vincent@example.com',
-        role: 'Business Relations',
-        imageUrl:
-            'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-        lastSeen: null,
-    },
-    {
-        name: 'Lindsay Walton',
-        email: 'lindsay.walton@example.com',
-        role: 'Front-end Developer',
-        imageUrl:
-            'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-        lastSeen: '3h ago',
-        lastSeenDateTime: '2023-01-23T13:23Z',
-    },
-    {
-        name: 'Courtney Henry',
-        email: 'courtney.henry@example.com',
-        role: 'Designer',
-        imageUrl:
-            'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-        lastSeen: '3h ago',
-        lastSeenDateTime: '2023-01-23T13:23Z',
-    },
-    {
-        name: 'Tom Cook',
-        email: 'tom.cook@example.com',
-        role: 'Director of Product',
-        imageUrl:
-            'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-        lastSeen: null,
-    },
-]
-    }
-  },
 };
-/* antes de ingresar people */
 
 </script>
