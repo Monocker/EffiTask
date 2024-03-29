b5b7c781bc0c50a83c1fb75af5239bcd94504012
RicardoGH8 <ricardo.garcia.h87@gmail.com>
2024-03-29 15:53:13 -0500

    Dividir componentes de la vista Team

--- src/views/team/TeamView.vue
+++ src/views/team/TeamView.vue
@@ -6,56 +6,8 @@
         <div class="lg:col-span-5 xl:col-span-6 flex flex-col">
             <div class="relative z-10 rounded-xl bg-white shadow-xl overflow-hidden my-auto xl:mt-18">
                 <section>
-                    <header class="rounded-t-xl space-y-4 p-4 sm:px-8 sm:py-6 lg:p-4 xl:px-8 xl:py-6">
-                        <div class="flex items-center justify-between">
-                            <h2 class="font-semibold text-gray-900">Collaborators</h2>
-                            <div
-                                class="group flex items-center rounded-md bg-blue-500 text-white text-sm font-medium pl-2 pr-3 py-2 cursor-pointer shadow-sm hover:bg-blue-400">
-                                <svg width="20" height="20" fill="currentColor" class="mr-2">
-                                    <path
-                                        d="M10 5a1 1 0 0 1 1 1v3h3a1 1 0 1 1 0 2h-3v3a1 1 0 1 1-2 0v-3H6a1 1 0 1 1 0-2h3V6a1 1 0 0 1 1-1Z">
-                                    </path>
-                                </svg>New
-                            </div>
-                        </div>
-                        <div class="group relative rounded-md">
-                            <svg width="20" height="20" fill="currentColor"
-                                class="absolute left-3 top-1/2 -mt-2.5 text-gray-400 pointer-events-none group-focus-within:text-blue-500">
-                                <path fill-rule="evenodd" clip-rule="evenodd"
-                                    d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z">
-                                </path>
-                            </svg>
-                            <input type="text" aria-label="Filter projects" placeholder="Filter collaborators..."
-                                class="appearance-none w-full text-sm leading-6 bg-transparent text-gray-900 placeholder-gray-400 rounded-md py-2 pl-10 ring-1 ring-gray-200 shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500">
-                        </div>
-                    </header>
-                    <div class="px-4 py-2">
-                        <ul role="list" class="divide-y divide-gray-100">
-                            <li v-for="person in people" :key="person.email" class="flex justify-between gap-x-6 py-5">
-                                <div class="flex min-w-0 gap-x-4">
-                                    <img class="h-12 w-12 flex-none rounded-full bg-gray-50" :src="person.imageUrl"
-                                        alt="" />
-                                    <div class="min-w-0 flex-auto">
-                                        <p class="text-sm font-semibold leading-6 text-gray-900">{{ person.name }}</p>
-                                        <p class="mt-1 truncate text-xs leading-5 text-gray-500">{{ person.email }}</p>
-                                    </div>
-                                </div>
-                                <div class="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
-                                    <p class="text-sm leading-6 text-gray-900">{{ person.role }}</p>
-                                    <p v-if="person.lastSeen" class="mt-1 text-xs leading-5 text-gray-500">
-                                        Last seen <time :datetime="person.lastSeenDateTime">{{ person.lastSeen }}</time>
-                                    </p>
-                                    <div v-else class="mt-1 flex items-center gap-x-1.5">
-                                        <div class="flex-none rounded-full bg-emerald-500/20 p-1">
-                                            <div class="h-1.5 w-1.5 rounded-full bg-emerald-500" />
-                                        </div>
-                                        <p class="text-xs leading-5 text-gray-500">Online</p>
-                                    </div>
-                                </div>
-                            </li>
-                        </ul>
-                    </div>
-
+                    <THeaderComponent/>
+                    <TListComponent :people="people" :user="user" />
                 </section>
             </div>
         </div>
@@ -75,6 +27,8 @@
 //import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue'
 //import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/vue/24/outline'
 import NavBarComponent from '../../components/home/dashboard/NavBarComponent.vue'
+import THeaderComponent from '../../components/team/THeaderComponent.vue'
+import TListComponent from '../../components/team/TListComponent.vue'
 import { ref, onMounted } from 'vue';
 
 // Define la variable reactiva dentro de setup
@@ -95,7 +49,9 @@ const closeMenuOnClickOutside = (event) => {
 
 export default {
   components: {
-    NavBarComponent
+    NavBarComponent,
+    THeaderComponent,
+    TListComponent
   },
   data() {
     return {
