73aa4c091a7ccc55c2a6f2228c17b13d86182834
RicardoGH8 <ricardo.garcia.h87@gmail.com>
2024-03-29 14:33:53 -0500

    Se finalizo la tarea del gei de Portal

---
+++ src/components/home/teams/HeaderTeamComponent.vue
@@ -0,0 +1,25 @@
+<template>
+    <header class="rounded-t-xl space-y-4 p-4 sm:px-8 sm:py-6 lg:p-4 xl:px-8 xl:py-6">
+        <div class="flex items-center justify-between">
+        <h2 class="font-semibold text-gray-900">Projects</h2>
+        <div class="group flex items-center rounded-md bg-blue-500 text-white text-sm font-medium pl-2 pr-3 py-2 cursor-pointer shadow-sm hover:bg-blue-400">
+            <svg width="20" height="20" fill="currentColor" class="mr-2">
+            <path d="M10 5a1 1 0 0 1 1 1v3h3a1 1 0 1 1 0 2h-3v3a1 1 0 1 1-2 0v-3H6a1 1 0 1 1 0-2h3V6a1 1 0 0 1 1-1Z"></path>
+            </svg>New
+        </div>
+        </div>
+        <div class="group relative rounded-md">
+        <svg width="20" height="20" fill="currentColor" class="absolute left-3 top-1/2 -mt-2.5 text-gray-400 pointer-events-none group-focus-within:text-blue-500">
+            <path fill-rule="evenodd" clip-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"></path>
+        </svg>
+        <input type="text" aria-label="Filter projects" placeholder="Filter projects..." class="appearance-none w-full text-sm leading-6 bg-transparent text-gray-900 placeholder-gray-400 rounded-md py-2 pl-10 ring-1 ring-gray-200 shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500">
+        </div>
+    </header>
+    </template>
+    
+    <script>
+    </script>
+    
+    <style>
+    </style>
+    
--- src/views/home/HomeView.vue
+++ src/views/home/HomeView.vue
@@ -100,54 +100,8 @@
         <div class="lg:col-span-5 xl:col-span-6 flex flex-col">
             <div class="relative z-10 rounded-xl bg-white shadow-xl overflow-hidden my-auto xl:mt-18">
                 <section>
-                    <header class="rounded-t-xl space-y-4 p-4 sm:px-8 sm:py-6 lg:p-4 xl:px-8 xl:py-6">
-                        <div class="flex items-center justify-between">
-                            <h2 class="font-semibold text-gray-900">Projects</h2>
-                            <div
-                                class="group flex items-center rounded-md bg-blue-500 text-white text-sm font-medium pl-2 pr-3 py-2 cursor-pointer shadow-sm hover:bg-blue-400">
-                                <svg width="20" height="20" fill="currentColor" class="mr-2">
-                                    <path
-                                        d="M10 5a1 1 0 0 1 1 1v3h3a1 1 0 1 1 0 2h-3v3a1 1 0 1 1-2 0v-3H6a1 1 0 1 1 0-2h3V6a1 1 0 0 1 1-1Z">
-                                    </path>
-                                </svg>New
-                            </div>
-                        </div>
-                        <div class="group relative rounded-md">
-                            <svg width="20" height="20" fill="currentColor"
-                                class="absolute left-3 top-1/2 -mt-2.5 text-gray-400 pointer-events-none group-focus-within:text-blue-500">
-                                <path fill-rule="evenodd" clip-rule="evenodd"
-                                    d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z">
-                                </path>
-                            </svg>
-                            <input type="text" aria-label="Filter projects" placeholder="Filter projects..."
-                                class="appearance-none w-full text-sm leading-6 bg-transparent text-gray-900 placeholder-gray-400 rounded-md py-2 pl-10 ring-1 ring-gray-200 shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500">
-                        </div>
-                    </header>
-                    <ul
-                        class="bg-gray-50 p-4 sm:px-8 sm:pt-6 sm:pb-8 lg:p-4 xl:px-8 xl:pt-6 xl:pb-8 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-1 xl:grid-cols-2 gap-4 text-sm leading-6">
-                        <li
-                            class="group cursor-pointer rounded-md p-3 bg-white ring-1 ring-gray-200 shadow-sm hover:bg-blue-500 hover:ring-blue-500 hover:shadow-md">
-                            <dl class="grid sm:block lg:grid xl:block grid-cols-2 grid-rows-2 items-center">
-                                <div>
-                                    <dt class="sr-only">Title</dt>
-                                    <dd class="font-semibold text-gray-900 group-hover:text-white">API Integration</dd>
-                                </div>
-                                <div>
-                                    <dt class="sr-only">Category</dt>
-                                    <dd class="group-hover:text-blue-200">Engineering</dd>
-                                </div>
-                                <div class="col-start-2 row-start-1 row-end-3">
-                                    <dt class="sr-only">Users</dt>
-                                    <dd class="flex justify-end -space-x-1.5">
-                                        <img src="https://images.unsplash.com/photo-1531123897727-8f129e1688ce?auto=format&fit=facearea&facepad=2&w=48&h=48&q=80"
-                                            alt="" class="w-6 h-6 rounded-full bg-gray-100 ring-2 ring-white">
-                                        <!-- Otros avatares -->
-                                    </dd>
-                                </div>
-                            </dl>
-                        </li>
-                        <!-- Más elementos de la lista -->
-                    </ul>
+                    <HeaderTeamComponent/>
+                    <ListTeamComponent/>
                 </section>
             </div>
         </div>
@@ -168,12 +122,16 @@
 //import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/vue/24/outline'
 
 import NavBarComponent from '../../components/home/dashboard/NavBarComponent.vue'
+import HeaderTeamComponent from '../../components/home/teams/HeaderTeamComponent.vue';
+import ListTeamComponent from '../../components/home/teams/ListTeamComponent.vue';
 /* onMo */
 
 /*  */
 export default {
   components: {
-    NavBarComponent
+    NavBarComponent,
+    HeaderTeamComponent,
+    ListTeamComponent
   },
   data() {
     return {
