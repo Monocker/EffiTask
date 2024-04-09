2793d0f74972d8a5feac4eb1d5da2977ce6160f9
Monocker <36136678+Monocker@users.noreply.github.com>
2024-04-09 18:19:55 -0500

    Developer (#19)
    * Rama feature-calendar
    
    * Fix, Team - Enviar email, para que se pueda terminar registrar el colaborado.
    
    * Fix, Team - Validación para que no se repita correo en colaboradores.
    
    * Fix, Estilos del correo que se manda
    
    * Fix, Envío de correo para Restablecer contraseña.
    
    * Fix. Team - Modal para editar colaborador funcional.
    
    * Fix, Team - Correcion de modales.
    
    * Feat, Account. Creacion de la vista y la ruta.
    
    * Feat, Vista Account y traer los departamentos del manager
    
    * Fix, team - validación filter
    
    * Fix, agregar array en register
    
    * Integracion de Activity en el Navbar
    
    ---------
    
    Co-authored-by: YadiraHC <106407754+YadiraHC@users.noreply.github.com>

--- src/core/services/authentication/AuthentificationService.ts
+++ src/core/services/authentication/AuthentificationService.ts
@@ -15,7 +15,8 @@ const registerUser = async (
   email: string,
   password: string,
   companyName: string,
-  ownerName: string
+  ownerName: string,
+  departments: string[] = []
 ) => {
   const userCredential = await createUserWithEmailAndPassword(auth, email, password)
   const user = userCredential.user
@@ -25,6 +26,7 @@ const registerUser = async (
     ownerName: ownerName,
     email: email,
     role: 'manager',
+    departments: departments,
     createdAt: new Date()
   })
 }
