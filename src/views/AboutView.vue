2bdb92d2c92c348a4913c9565b365c98e98b20c2
1Don2angello <106407139+1Don2angello@users.noreply.github.com>
2024-04-12 11:28:25 -0500

    Fix - Landing Page. Estilos y rutas.

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,71 +2,39 @@
   <div>
     <nav-bar-component />
     <!--  <hero-section-component /> -->
-    <section class="text-gray-700 body-font">
-      <div class="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
-        <div class="text-center lg:w-2/3 w-full">
+    <section class="bg-[#edf2fa] text-gray-700 body-font overflow-hidden">
+      <div class="container mx-auto flex px-5 py-24 items-center justify-between">
+        <!-- Contenido de texto -->
+        <div class="flex-1 flex flex-col md:items-start md:text-left items-center text-center px-4">
           <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
             We’re here to Increase your Productivity
           </h1>
           <p class="mb-8 leading-relaxed">
-            Let’s make your work more organic and easily using the EffiTask Dashboard with many of the latest features
-            in
-            managing work every day.
+            Let’s make your work more organized and easily using the EffiTask Dashboard with many of the latest features
+            in managing work every day.
           </p>
           <div class="flex justify-center">
             <button
               class="inline-flex text-white bg-blue-500 border-0 py-2 px-6 focus:outline-none hover:bg-blue-600 rounded text-lg">
-              <router-link to="/register">
-                Try free
-              </router-link>
+              <router-link to="/register">Try free trial</router-link>
             </button>
-
             <button
               class="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">
-              <router-link to="/login">
-                LogIn
-              </router-link>
+              <router-link to="/login">LogIn</router-link>
             </button>
           </div>
+        </div>
+        <!-- Imagen -->
+        <div class="flex-1 hidden lg:block">
+          <img class="object-cover object-center rounded" alt="hero"
+            src="https://cdn.dribbble.com/userupload/4000360/file/original-b669b77dcb9663a55944d7864d09100a.jpg?resize=752x">
         </div>
       </div>
     </section>
     <!-- End Hero -->
 
     <!--  <features-section-component /> -->
-    <section class="text-gray-600 body-font">
-      <div class="container px-5 py-24 mx-auto">
-        <div class="flex flex-wrap -m-4">
-          <div class="p-4 md:w-1/3">
-            <div class="flex rounded-lg h-full bg-gray-100 p-8 flex-col">
-              <div class="flex items-center mb-3">
-                <div
-                  class="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-blue-500 text-white flex-shrink-0">
-                  <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                    class="w-5 h-5" viewBox="0 0 24 24">
-                    <!-- Icon here -->
-                  </svg>
-                </div>
-                <h2 class="text-gray-900 text-lg title-font font-medium">Collaboration Teams</h2>
-              </div>
-              <div class="flex-grow">
-                <p class="leading-relaxed text-base">Here you can handle projects together with your team virtually.</p>
-              </div>
-            </div>
-          </div>
-          <div class="p-4 md:w-1/3">
-            <div class="flex rounded-lg h-full bg-gray-100 p-8 flex-col">
-              <!-- ... -->
-            </div>
-          </div>
-          <div class="p-4 md:w-1/3">
-            <div class="flex rounded-lg h-full bg-gray-100 p-8 flex-col">
-              <!-- ... -->
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
+
     <!-- End Feature-Section -->
 
     <!-- <dashboard-preview-component /> -->
